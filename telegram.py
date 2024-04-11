fa7d8de3619f29a11f4e7112d2c4e70cbbb61803
Sajjadabdinasab <reza.sasan2353@gmail.com>
2024-04-11 16:11:49 +0330

    Update Code
    Now you can use easily

--- telegram.py
+++ telegram.py
@@ -1,4 +1,3 @@
-# Test
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -8,38 +7,32 @@ if __name__ == '__main__':
     driver = webdriver.Firefox()
     driver.get("https://web.telegram.org/a")
 
-runner = 0
+
 def telegram():
     global driver
-    def sender():
-        global driver
-        text = input('Enter your txt : ')
-        count = int(input('How often do you want to be repeated : '))
-        driver = driver.find_element(By.XPATH,'//*[@id="editable-message-text"]')
-        for i in range(count):
-            driver.send_keys(text)
-            driver.send_keys(Keys.ENTER)
-            time.sleep(.2)
-        driver.send_keys('CREATOR : ---Mohammad Sajjad Abdinasab---')
-        print('Successfully')
-    if runner == 0:
-        user_name = input('Enter username : ')
-        driver = driver.find_element(By.ID,'telegram-search-input')
-        driver.send_keys('{}'.format(user_name))
-        time.sleep(1)
+    text = input('Enter your txt : ')
+    while True:
+        try:
+            count = int(input('How often do you want to be repeated : '))
+            break
+        except ValueError:
+            print('Please Just Enter Number')
+    driver = driver.find_element(By.XPATH,'//*[@id="editable-message-text"]')
+    for i in range(count):
+        driver.send_keys(text)
         driver.send_keys(Keys.ENTER)
-        sender()
-    else:
-        sender()
-   
+        time.sleep(.2)
+    driver.send_keys('CREATOR : ---Mohammad Sajjad Abdinasab---')
+    print('Successfully')
+    
 if __name__ == '__main__':   
     telegram()
-    runner = 1
     while True:
         ask = input('Run again (y/n) : ')
-        if ask == 'y':
+        if ask.lower() == 'y':
+            driver.clear()
             telegram()
-        elif ask == 'n':
+        elif ask.lower() == 'n':
             print('Good luck')
             break
         else:
